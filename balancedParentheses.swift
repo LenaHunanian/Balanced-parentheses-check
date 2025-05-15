043d83316517ae63a3d0471c1388c3a14a2a97ad
LenaHunanian <lenasss20053112@gmail.com>
2025-05-15 12:56:22 +0400

    Create balancedParentheses.swift on main
    Added public function balancedParentheses which checks the validness of parentheses

---
+++ balancedParentheses.swift
@@ -0,0 +1,14 @@
+public func balancedParentheses(input: String) -> Bool {
+  var stack = [Character]()
+  for char in input {
+    if char == "(" {
+      stack.append(char)
+    }else if char == ")"{
+      if stack.isEmpty{
+        return false
+      }
+      stack.removeLast()
+    }
+  }
+  return stack.isEmpty
+}
